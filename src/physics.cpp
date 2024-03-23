e93d0f87b50f37a88e1f42cfcc38e36266d93da4
periaraperi <periaraperi@protonmail.com>
2024-03-24 01:57:44 +0400

    removed extra checks

--- src/physics.cpp
+++ src/physics.cpp
@@ -11,10 +11,8 @@ bool aabb(const AABB_Collider &a, const AABB_Collider &b)
     const auto& bw = b.size.x;
     const auto& bh = b.size.y;
     
-    bool overlap_x = (ax <= bx && bx <= (ax+aw)) || (ax <= (bx+bw) && (bx+bw) <= (ax+aw)) || 
-                     (ax <= bx && (bx+bw) <= (ax+aw)) || (ax >= bx && (bx+bw) >= (ax+aw));
-    bool overlap_y = (ay >= by && by >= (ay-ah)) || (ay >= (by-bh) && (by-bh) >= (ay-ah)) ||
-                     (ay >= by && (by-bh) >= (ay-ah)) || (ay <= by && (by-bh) <= (ay-ah));
+    bool overlap_x = (ax <= (bx+bw)) && ((ax+aw) >= bx);
+    bool overlap_y = (ay >= (by-bh)) && ((ay-ah) <= by);
 
     return overlap_x && overlap_y;
 }
